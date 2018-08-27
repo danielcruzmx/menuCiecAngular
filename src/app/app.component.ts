529b14911744fbec23d6758f10d8d23343ace073
danielcruzmx <danielcruzmx@hotmail.com>
2018-08-26 20:16:00 -0500

    cambios en estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,16 +16,31 @@ export class AppComponent {
       this.title = "CIEC Cuotas, Ingresos y Egresos ";
       this.menuList = [
         {
-          "name": "OLIMPO",
-          "subMenu": ["Captura de movimientos", "Depositos y retiros por mes", "Cargos, depositos y cuotas"]
+          "name": "CIEC",
+          "subMenu": [["Cambio de periodos","http://192.168.0.60:8000/admin/main/periodo/"],
+                      ["Cierre de sesión y cambio de identificación","http://192.168.0.60:8000/admin/logout/"]
+        ]
         },
         {
-          "name": "SADICARNOT 10",
-          "subMenu": ["Captura de movimientos", "Depositos y retiros por mes", "Cargos, depositos y cuotas"]
+          "name": "OLIMPO",
+          "subMenu": [["Captura de movimientos","http://192.168.0.60:8000/admin/c_olimpo/movimiento/"],
+                      ["Depositos y retiros por mes","http://192.168.0.60:8000/explorer/1/download?format=csv"],
+                      ["Cargos, depositos y cuotas por partida","http://192.168.0.60:8000/admin/c_olimpo/condomino/"]
+                     ]
         },
         {
           "name": "SADICARNOT 81",
-          "subMenu": ["Captura de movimientos", "Depositos y retiros por mes", "Cargos, depositos y cuotas"]
+          "subMenu": [["Captura de movimientos","http://192.168.0.60:8000/admin/c_sadiochouno/movimiento/"],
+                      ["Depositos y retiros por mes","http://192.168.0.60:8000/explorer/23/download?format=csv"],
+                      ["Cargos, depositos y cuotas por partida","http://192.168.0.60:8000/admin/c_sadiochouno/condomino/"]
+                     ]
+        },
+        {
+          "name": "SADICARNOT 10",
+          "subMenu": [["Captura de movimientos","http://192.168.0.60:8000/admin/c_sadicarnot/movimiento/"],
+                      ["Depositos y retiros por mes","http://192.168.0.60:8000/explorer/14/download?format=csv"],
+                      ["Cargos, depositos y cuotas por partida","http://192.168.0.60:8000/admin/c_sadicarnot/condomino/"]
+                     ]
         }
       ]
     }
